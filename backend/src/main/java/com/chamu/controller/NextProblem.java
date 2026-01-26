223bd4d24127e6df4c8021e1f5b118f3c988af19
chamuditha-t <chamudithatheekshana6815@gmail.com>
2026-01-26 23:40:13 +0530

    3rd commit

--- backend/src/main/java/com/chamu/controller/NextProblem.java
+++ backend/src/main/java/com/chamu/controller/NextProblem.java
@@ -1,9 +1,18 @@
 package com.chamu.controller;
 
+import com.chamu.entity.Problem;
+import com.chamu.entity.Status;
 import com.chamu.entity.User;
+import com.chamu.entity.UserProgress;
+import com.chamu.util.HibernateUtil;
 import com.chamu.util.TokenUtil;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,6 +20,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/nextProblem")
 public class NextProblem extends HttpServlet {
@@ -28,6 +38,8 @@ public class NextProblem extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
 
+        System.out.println("=== getNextProblem API called ===");
+
         resp.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
         resp.setHeader("Access-Control-Allow-Credentials", "true");
         resp.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
@@ -39,10 +51,76 @@ public class NextProblem extends HttpServlet {
         JsonObject response = new JsonObject();
         response.addProperty("status", false);
 
+        // 🔹 TOKEN
         String authHeader = req.getHeader("Authorization");
+        System.out.println("Authorization Header: " + authHeader);
+
         String token = authHeader.replace("Bearer ", "");
         User user = TokenUtil.validateToken(token);
 
-        System.out.println(user.getEmail());
+        System.out.println("Token validated for user email: " + user.getEmail());
+
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        System.out.println("Hibernate session opened");
+
+        // 🔹 USER
+        Criteria criteria = session.createCriteria(User.class);
+        criteria.add(Restrictions.eq("email", user.getEmail()));
+        User selectedUser = (User) criteria.uniqueResult();
+
+        System.out.println("Selected User ID: " + selectedUser.getId());
+
+        // 🔹 STATUS
+        Criteria statusCriteria = session.createCriteria(Status.class);
+        statusCriteria.add(Restrictions.eq("status", "Completed"));
+        Status selectedStatus = (Status) statusCriteria.uniqueResult();
+
+        System.out.println("Selected Status: " + selectedStatus.getStatus());
+
+        // 🔹 USER PROGRESS
+        Criteria progressCriteria = session.createCriteria(UserProgress.class);
+        progressCriteria.add(Restrictions.eq("user", selectedUser));
+        progressCriteria.add(Restrictions.eq("status", selectedStatus));
+        progressCriteria.setProjection(Projections.max("problem.id"));
+
+        Integer lastSolvedProblemId =
+                (Integer) progressCriteria.uniqueResult();
+
+        System.out.println("Last Solved Problem ID: " + lastSolvedProblemId);
+
+        // 🔹 NEXT PROBLEM
+        Criteria problemCriteria = session.createCriteria(Problem.class);
+
+        if (lastSolvedProblemId != null) {
+            System.out.println("User has solved problems. Finding next problem...");
+            problemCriteria.add(Restrictions.gt("id", lastSolvedProblemId));
+        } else {
+            System.out.println("User has not solved any problem. Getting first problem...");
+        }
+
+        problemCriteria.addOrder(Order.asc("id"));
+        problemCriteria.setMaxResults(1);
+
+        Problem nextProblem =
+                (Problem) problemCriteria.uniqueResult();
+
+        if (nextProblem != null) {
+            System.out.println("Next Problem Found:");
+            System.out.println("Problem ID: " + nextProblem.getId());
+            System.out.println("Problem Title: " + nextProblem.getTitle());
+
+            response.addProperty("problemId", nextProblem.getId());
+            response.addProperty("status", true);
+        } else {
+            System.out.println("No next problem found. Course completed.");
+            response.addProperty("courseCompleted", true);
+        }
+
+        session.close();
+        System.out.println("Hibernate session closed");
+
+        resp.getWriter().write(gson.toJson(response));
+        System.out.println("=== Response sent ===");
     }
+
 }
